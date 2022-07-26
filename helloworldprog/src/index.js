892e4dc70b88b89e9e94f0a0f97ade68cdee2724
sagunaukade <sagunaukade96@gmail.com>
2022-07-26 15:09:18 +0530

    [Saguna]:-Added in template literals program

--- helloworldprog/src/index.js
+++ helloworldprog/src/index.js
@@ -2,13 +2,19 @@ var React=require('react');//simple
 //import React from 'react';
 var ReactDOM= require('react-dom');
 //import ReactDOM from 'react-dom';
-const name="Pallavi Pawar";
+const fname="Saguna";
+const lname="Ukade";
+
 //array element
 //const arr=['Saguna' , 'Ukade']
 ReactDOM.render(
  // <div>
 <React.Fragment>
-<h1> my name is {name}</h1>
+ 
+<h1> my name is {fname} {lname}</h1>
+<h1> my name is {fname + " " + lname}</h1>
+<h1> my name is {`${fname} ${lname}`}</h1>
+
 
 <p> add of two num is {5+5}</p>
 <p> random num is {Math.random()}</p>
