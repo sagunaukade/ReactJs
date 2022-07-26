687cb29ae25d998360b0aaaf6b505c057482d1bc
sagunaukade <sagunaukade96@gmail.com>
2022-07-26 15:18:21 +0530

    [Saguna]:-Added Current date and time in jsx in reactjs

--- helloworldprog/src/index.js
+++ helloworldprog/src/index.js
@@ -2,8 +2,10 @@ var React=require('react');//simple
 //import React from 'react';
 var ReactDOM= require('react-dom');
 //import ReactDOM from 'react-dom';
-const fname="Saguna";
-const lname="Ukade";
+const name="Saguna";
+const currDate=new Date().toLocaleDateString();
+const currTime=new Date().toLocaleTimeString();
+// const lname="Ukade";
 
 //array element
 //const arr=['Saguna' , 'Ukade']
@@ -11,13 +13,16 @@ ReactDOM.render(
  // <div>
 <React.Fragment>
  
-<h1> my name is {fname} {lname}</h1>
+{/* <h1> my name is {fname} {lname}</h1>
 <h1> my name is {fname + " " + lname}</h1>
-<h1> my name is {`${fname} ${lname}`}</h1>
+<h1> my name is {`${fname} ${lname}`}</h1> */}
 
+<h1>Hello My name is {name}</h1>
+<p>Current date is={currDate}</p>
+<p>Current time is={currTime}</p>
 
-<p> add of two num is {5+5}</p>
-<p> random num is {Math.random()}</p>
+{/* <p> add of two num is {5+5}</p>
+<p> random num is {Math.random()}</p> */}
 
 </React.Fragment>
 //</div>
