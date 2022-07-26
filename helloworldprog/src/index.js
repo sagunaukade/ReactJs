76311be2b5c11c32a01c36ae43591a57febe3c14
sagunaukade <sagunaukade96@gmail.com>
2022-07-26 14:58:16 +0530

    [Saguna]:-Added in render multiple elements program

--- helloworldprog/src/index.js
+++ helloworldprog/src/index.js
@@ -1,5 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+var React=require('react');//simple
+//import React from 'react';
+var ReactDOM= require('react-dom');
+//import ReactDOM from 'react-dom';
+const name="Pallavi Pawar";
+//array element
+//const arr=['Saguna' , 'Ukade']
+ReactDOM.render(
+ // <div>
+<React.Fragment>
+<h1> my name is {name}</h1>
 
-ReactDOM.render(<h1> Hello World</h1>,
-document.getElementById("root"));
+<p> add of two num is {5+5}</p>
+<p> random num is {Math.random()}</p>
+
+</React.Fragment>
+//</div>
+,
+document.getElementById('root'));
