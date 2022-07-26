35802329f0c0911a538881955ae682bd3e6808f5
sagunaukade <sagunaukade96@gmail.com>
2022-07-26 16:10:32 +0530

    [Saguna]:-Added css styling and importing css files in reactjs

--- helloworldprog/src/index.js
+++ helloworldprog/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import './index.css';
 const name="Saguna";
 const img1="https://picsum.photos/200/300";
 const img2="https://picsum.photos/200/300";
@@ -10,13 +10,20 @@ const links="https://picsum.photos/200/300"
 
 ReactDOM.render(
     <>
-<h1 contentEditable="true">My Name Is {name}</h1>
+<h1 className="heading">My Name Is {name}</h1>
 {/* <img src="https://picsum.photos/200/300" alt="RandomImeges"/> */}
+<div className="img_div">
 <img src={img1} alt="randomimages"/>
 <img src={img2} alt="randomimages"/>
 <img src={img3} alt="randomimages"/>
-<a href={links} target="_Saguna"></a>
+<a href={links} target="_Saguna">
 <img src={img4} alt="randomimages"/>
+</a>
+</div>
+
+{/* <img src={img3} alt="randomimages"/>
+<a href={links} target="_Saguna"></a>
+<img src={img4} alt="randomimages"/> */}
 
 </>,
 document.getElementById("root"));
